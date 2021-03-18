8bc6a0fc60bba3b08a7bb29bbb1cd7e196e97050
GaiaGuzzoBonifacio <gaia.guzzobonifacio@gmail.com>
2021-03-18 23:17:30 +0100

    eh

--- TaskListArrayApp-main/lib/searchFunctions.php
+++ TaskListArrayApp-main/lib/searchFunctions.php
@@ -4,19 +4,23 @@ function searchText($searchText) {
     
    return function ($taskItem) use ($searchText) {
        
-    
+    //sanitize
     $noSpaces = preg_replace('/[ ]+/m', ' ', $searchText);
        
        $stringToLowerCase = strtolower($taskItem['taskName']);
        
        $searchToLowerCase = trim(strtolower($noSpaces));
        
-       
+    //fixare tutti i risultati   
        if ($searchToLowerCase !== '') {
-           $result = strpos($stringToLowerCase, $searchToLowerCase) !== false;
-       } else {
-           $result = true;
-       }
+           
+        $result = strpos($stringToLowerCase, $searchToLowerCase) !== false;
+       
+    } else {
+           
+        $result = true;
+       
+    }
        
        
        return $result;
@@ -24,8 +28,9 @@ function searchText($searchText) {
    
 }
 
-function searchStatus(string $search): callable {
+function searchStatus(string $search) {
     
+
     return function ($mockTaskItem) use ($search) {
         
         if ($search !== '') {
