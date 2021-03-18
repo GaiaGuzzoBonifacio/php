75807905751765cd6b5fcbe8116b6bd2c8171280
GaiaGuzzoBonifacio <gaia.guzzobonifacio@gmail.com>
2021-03-18 23:12:31 +0100

    ah

--- TaskListArrayApp-main/lib/searchFunctions.php
+++ TaskListArrayApp-main/lib/searchFunctions.php
@@ -3,30 +3,53 @@
 function searchText($searchText) {
     
    return function ($taskItem) use ($searchText) {
-       $noSpaces = preg_replace('/[ ]+/m', ' ', $searchText);
+       
+    
+    $noSpaces = preg_replace('/[ ]+/m', ' ', $searchText);
+       
        $stringToLowerCase = strtolower($taskItem['taskName']);
+       
        $searchToLowerCase = trim(strtolower($noSpaces));
+       
+       
        if ($searchToLowerCase !== '') {
            $result = strpos($stringToLowerCase, $searchToLowerCase) !== false;
        } else {
            $result = true;
        }
+       
+       
        return $result;
    };
    
 }
 
 function searchStatus(string $search): callable {
+    
     return function ($mockTaskItem) use ($search) {
+        
         if ($search !== '') {
+            
+            
             if ($search !== 'all') {
+                
                 $result = strpos($mockTaskItem['status'], $search) !== false;
+            
+            
             } else {
+                
                 $result = true;
+           
             }
+        
+        
         } else {
+            
             $result = true;
+        
         }
+       
+       
         return $result;
     };
 }
